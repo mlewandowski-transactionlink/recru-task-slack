e9eae127ed13bd2eda325676e2ad6bd3f0ba233c
nikowis <nikowis@gmail.com>
2021-01-07 21:21:12 +0100

    Empty spring boot project

---
+++ src/main/java/pl/nikowis/insult/InsultApplication.java
@@ -0,0 +1,13 @@
+package pl.nikowis.insult;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsultApplication.class, args);
+    }
+
+}
